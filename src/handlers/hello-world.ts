243e7a56171ddd672407f8941d7a0aa7f2ba1101
Harry McGoldrick <harry_mcgoldrick@outlook.com>
2024-07-11 14:01:32 +0100

    add api response

--- src/handlers/hello-world.ts
+++ src/handlers/hello-world.ts
@@ -2,8 +2,9 @@ import {logger} from "../utils/logger";
 import {PutItemCommand, PutItemCommandInput} from "@aws-sdk/client-dynamodb";
 import {dynamoClient} from "../aws-clients/dynamo";
 import {marshall} from "@aws-sdk/util-dynamodb";
+import {APIGatewayProxyResult} from "aws-lambda";
 
-export const handler = async (request: unknown): Promise<string> => {
+export const handler = async (request: unknown): Promise<APIGatewayProxyResult> => {
     try {
         const item: PutItemCommandInput = {
             Item: marshall({pk: 'hello', sk: 'world!'}),
@@ -12,7 +13,7 @@ export const handler = async (request: unknown): Promise<string> => {
 
         await dynamoClient().send(new PutItemCommand(item));
         logger.info('Successfully put item into hello world table', {item})
-        return 'Success';
+        return {statusCode: 200, body: JSON.stringify({message: 'success'})};
     } catch (e) {
         logger.error('Failed putting item into hello world table', {error: e})
     }
